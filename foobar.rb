f0fcfd6145f7ee7a84f8d277d6e149b71432f4c1
Mike Lee <mike.lee@berkeley.edu>
2018-02-15 15:53:57 -0800

    done

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,9 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a = a.map { |e| e.to_i  }
+    a = a.find_all { |e| e % 2 == 0 && e < 10 }
+    return a.map { |e| e + 2 }.reduce(:+)
+
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a + b)**2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  return a.sort.map { |e| e + 1 }
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return first_name + ' ' + last_name
 end
 
 def blockin_time(a)
@@ -48,5 +48,6 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
+  
+  return word.split(//).map { | e| values[e.to_sym] }.reduce(:+)
 end
